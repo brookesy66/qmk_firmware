b80a7ade033db73a4b510018bb0b337d6566372c
Will Brookes <will.brookes@planetinnovation.com.au>
2026-03-18 15:07:53 +1100

    Contra: fix right ctrl
    * Add right ctrl button instead of arrow key
    * Fix outdated keycodes

--- keyboards/contra/keymaps/brookesy66/keymap.c
+++ keyboards/contra/keymaps/brookesy66/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,      KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,    KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,      KC_A,    KC_S,    KC_D,    KC_F,       KC_G,    KC_H,    KC_J,         KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,    KC_N,    KC_M,         KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-    KC_LCTL, MO(_ADJUST), KC_LGUI, KC_LALT, MO(_LOWER),     SPC_ML,       MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, MO(_ADJUST), KC_LGUI, KC_LALT, MO(_LOWER),     SPC_ML,       MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RCTL
   ),
 
   /* Movement
@@ -59,15 +59,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MOVEMENT] = LAYOUT_planck_mit(
     _______,      KC_MUTE, KC_VOLD, KC_VOLU, KC_END,   KC_F5,   _______, KC_PGUP, _______, _______,  KC_HOME, KC_DEL,
     _______,      _______, _______, KC_PGDN, _______,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
-    KC_LSPO,      _______, KC_DEL,  _______, _______,  _______, _______, _______, _______, _______,  _______, KC_RSPC,
+    SC_LSPO,      _______, KC_DEL,  _______, _______,  _______, _______, _______, _______, _______,  _______, SC_RSPC,
     MO(_POINTER), _______, _______, _______, _______,     _______,       _______, _______, KC_MPRV,  KC_MNXT, KC_MPLY
   ),
 
   [_POINTER] = LAYOUT_planck_mit(
-    _______, _______, _______, _______, _______,   _______,   _______, _______, _______, _______,  _______, _______,
+    _______, _______, _______, _______, _______,   _______,  _______, _______, _______, _______,  _______, _______,
 
-    _______, _______, _______, _______, _______,  _______,   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-    _______, KC_BTN1, KC_BTN2, KC_BTN3, _______,  _______,   _______, _______, _______, _______,  _______, _______,
+    _______, _______, _______, _______, _______,  _______,   MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT,  _______, _______,
+    _______, MS_BTN1, MS_BTN2, MS_BTN3, _______,  _______,   _______, _______, _______, _______,  _______, _______,
     _______, _______, _______, _______, _______,       _______,       _______, _______, _______,  _______, _______
   ),
 
